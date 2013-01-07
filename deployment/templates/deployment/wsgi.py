f683291c406d9ad9e1a331f4e01ade335d78193e
Mark Jones <mjones@imagehawk.com>
2013-01-07 14:57:20 -0600

    Set the DJANGO_HOSTNAME in the wsgi file based on the servername options

--- deployment/templates/deployment/wsgi.py
+++ deployment/templates/deployment/wsgi.py
@@ -11,6 +11,9 @@ from django.core.handlers.wsgi import WSGIHandler
 #setup the environment for Django and Pinax
 #pinax.env.setup_environ(__file__, settings_path=os.path.abspath(os.path.join()
 
+{% if options.servername %}
+os.environ["DJANGO_HOSTNAME"] = {{ options.servername }}.split('.')[0]
+{% endif %}
 os.environ["DJANGO_SETTINGS_MODULE"] = '{{ settings.SETTINGS_MODULE }}'
 
 # set application for WSGI processing
