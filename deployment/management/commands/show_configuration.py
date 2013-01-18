8d361bbe1fc2b08dbc705e3d3bb0e525b4d1d924
Mark Jones <mjones@imagehawk.com>
2013-01-18 15:27:44 -0600

    Added commands for show_configuration and test_email

---
+++ deployment/management/commands/show_configuration.py
@@ -0,0 +1,38 @@
+import os, sys, pprint
+
+from django.core.management.base import BaseCommand, CommandError
+from optparse import make_option
+from django.conf import settings
+
+PROJECT_NAME = settings.SETTINGS_MODULE.split('.')[0]
+
+class Command(BaseCommand):
+    option_list = BaseCommand.option_list + (
+        make_option('--only',
+                    default=False,
+                    dest="only",
+                    action="store_true",
+                    help="only print out the named parameters, don't include the defaults"), )
+    help = ("Print out the configuration options in place on this computer")
+
+    def handle(self, *args, **options):
+
+        result = 0
+        if options["only"]:
+            display_params = []
+        else:
+            display_params = ["DEFAULT_FROM_EMAIL", "EMAIL_BACKEND",
+                              "EMAIL_HOST", "EMAIL_SUBJECT_PREFIX",
+                              "SESSION_COOKIE_SECURE", "DATABASES"]
+        for arg in args:
+            display_params.append(arg)
+
+        for param in display_params:
+            try:
+                print "%s: %s" % (param,
+                                  pprint.pformat(getattr(settings, param)))
+            except AttributeError:
+                print "%s: was not found in settings" % param
+                result = 1
+
+        return result
---
+++ deployment/management/commands/test_email.py
@@ -0,0 +1,63 @@
+import os, sys, pprint
+
+from django.core.management.base import BaseCommand, CommandError
+from optparse import make_option
+from django.conf import settings
+from django.core.mail import send_mail, mail_admins, mail_managers
+
+PROJECT_NAME = settings.SETTINGS_MODULE.split('.')[0]
+
+class Command(BaseCommand):
+    option_list = BaseCommand.option_list + (
+        make_option('--admins',
+                    default=False,
+                    dest="admins",
+                    action="store_true",
+                    help="Send a test email to the project admins"),
+        make_option('--managers',
+                    default=False,
+                    dest="managers",
+                    action="store_true",
+                    help="Send a test email to the project managers"),
+        make_option('--from',
+                    default=settings.DEFAULT_FROM_EMAIL,
+                    dest="from",
+                    help="Send a test email to the project managers"),
+        #make_option('--connection',
+                    #default=settings.EMAIL_BACKEND,
+                    #dest="connection",
+                    #help="Which backend to send mail with, defaults to"
+                    #" settings.EMAIL_BACKEND"),
+    )
+    help = ("Send a test email to the list of recipients from the specified"
+            " address, or using the DEFAULT_FROM_EMAIL if not specified,"
+            " from is ignored when sending to managers or admins")
+
+    def handle(self, *args, **options):
+
+        result = 0
+        subject = "Deployment Test Message"
+        message = "Deployment Test Message Body.  This was sent using" \
+                  " django_deployment to test the email configuration of a" \
+                  " deployed project."
+        sent_msg = False
+
+        if options["verbosity"] > "1":
+            print "Using %s" % settings.EMAIL_BACKEND
+
+        if options['admins']:
+            mail_admins(subject, message, fail_silently=False)
+            sent_msg = True
+        if options['managers']:
+            mail_managers(subject, message, fail_silently=False)
+            sent_msg = True
+
+        if args:
+            send_mail(subject, message, options["from"], args,
+                      fail_silently=False)
+        elif not sent_msg:
+            raise CommandError("No recipient addresses were specified on the"
+                               " command line")
+
+        return result
+
