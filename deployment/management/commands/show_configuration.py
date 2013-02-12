ef5932c7f180ccca756ac801fe3a4650814d5234
Greg Brown <gbrown@imagehawk.com>
2013-02-12 09:19:26 -0600

    Added --all to print out all settings

--- deployment/management/commands/show_configuration.py
+++ deployment/management/commands/show_configuration.py
@@ -13,14 +13,21 @@ class Command(BaseCommand):
                     default=False,
                     dest="only",
                     action="store_true",
-                    help="only print out the named parameters, don't include the defaults"), )
-    help = ("Print out the configuration options in place on this computer")
+                    help="only print out the named parameters, don't include the defaults"),
+        make_option('--all',
+                    default=False,
+                    dest="all",
+                    action="store_true",
+                    help="Print out all configuration parameters")    )
+    help = ("Print out the configuration options")
 
     def handle(self, *args, **options):
 
         result = 0
         if options["only"]:
             display_params = []
+        elif options["all"]:
+            display_params = dir(settings)
         else:
             display_params = ["DEFAULT_FROM_EMAIL", "EMAIL_BACKEND",
                               "EMAIL_HOST", "EMAIL_SUBJECT_PREFIX",
