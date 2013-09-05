0a1ece5affd9cf71e3f1c8f3018c1fc2311d98c8
Mark Jones <mjones@imagehawk.com>
2013-09-05 19:37:25 +0000

    Added ssl support and blocking to the default_wsgi.py file so I can augment it on a per setup basis.  Already using the {% block extra-environ %} in the cloudbox for lic on ssd

--- deployment/templates/deployment/default_wsgi.py
+++ deployment/templates/deployment/default_wsgi.py
@@ -1,14 +1,39 @@
-{% load deployment_tags %}import os, sys
+{% load deployment_tags %}
 
+{% block wsgi %}
+import os, sys
+
+{% block path %}
 sys.path = [
+{% block path-before %}
+{% endblock path-before %}
+
 {% for part in sys.path %}    "{{ part|flipslash }}",
 {% endfor %}
-]
 
+{% block path-after %}
+{% endblock path-after %}
+]
+{% endblock path %}
+
+{% block environ %}
 os.environ["DJANGO_HOSTNAME"] = '{% if options.server_name %}{{ options.server_name }}{% elif settings.ALLOWED_HOSTS %}{{ settings.ALLOWED_HOSTS|first }}{% else %}localhost{% endif %}'
 os.environ["DJANGO_SETTINGS_MODULE"] = '{{ settings.SETTINGS_MODULE }}'
 
+{% block extra-environ %}
+{% endblock extra-environ %}
+
+{% endblock environ %}
+
+# Additional
+{% block additional %}
+{% endblock additional %}
+
 # set application for WSGI processing
+{% block handler %}
 from django.core.handlers.wsgi import WSGIHandler
 
 application = WSGIHandler()
+{% endblock handler %}
+
+{% endblock wsgi %}
